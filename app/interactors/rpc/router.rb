426615346f0074471386e2916faff5fcc4db3006
Pavel Ishenin <isheninp@gmail.com>
2023-05-19 10:50:06 +0400

    error messages

--- app/interactors/rpc/router.rb
+++ app/interactors/rpc/router.rb
@@ -17,15 +17,26 @@ class Rpc::Router
       response = method.constantize.send(:call, context.props[:params])
       context.data = response.result
       context.status = response.success? ? 'success' : 'error'
+      if context.status == 'error'
+        context.error_message = response.error_message
+        context.error_code = response.error_code
+        context.error_id = response.error_id
+      end
     else
       context.status = 'error'
       context.data = 'wrong method'
+      context.error_message = 'wrong method'
+      context.error_code = 400
+      context.error_id = 400
     end
 
   rescue => e
 
     context.status = 'error'
     context.data = e.message
+    context.error_message = e.message
+    context.error_code = 500
+    context.error_id = 500
     context.fail!
  
   end
